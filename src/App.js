c120a8775547aa8a47ff9b1b0cd8596b9d4b312f
Samuel Soaga <your-email-address@example.com>
2023-05-23 17:09:50 +0100

    simple birthday-reminder app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import List from "./List";
+import data from "./data.js";
 
-function App() {
+const App = () => {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h2>{people.length} birthdays today</h2>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear List</button>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
